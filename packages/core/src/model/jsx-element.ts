acc2c65e3004ef351254690587fefd30cf9c934b
tbhuabi <464328895@qq.com>
2023-06-13 00:23:22 +0800

    fix: 还原不应该删除的代码

--- packages/core/src/model/jsx-element.ts
+++ packages/core/src/model/jsx-element.ts
@@ -56,13 +56,15 @@ export class JSXText {
 
 export type VNode = JSXElement | ComponentFactory | JSXText
 
-function flatChildren(jsxNodes: JSXChildNode[]) {
+function flatChildren(jsxNodes: JSXChildNode[] | JSXChildNode[][]) {
   const children: VNode[] = []
   for (const node of jsxNodes) {
     if (node instanceof JSXElement || typeof node === 'function') {
       children.push(node)
     } else if (typeof node === 'string' && node.length) {
       children.push(new JSXText(node))
+    } else if (Array.isArray(node)) {
+      children.push(...flatChildren(node))
     } else if (node !== null && typeof node !== 'undefined') {
       children.push(new JSXText(String(node)))
     }

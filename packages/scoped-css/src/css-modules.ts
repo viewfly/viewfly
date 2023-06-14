bf4f49467d5cdf35a3d2a7da0665661bb753d991
tbhuabi <464328895@qq.com>
2023-06-14 20:46:02 +0800

    feat: 模拟化 css 支持复杂配置

--- packages/scoped-css/src/css-modules.ts
+++ packages/scoped-css/src/css-modules.ts
@@ -3,9 +3,6 @@ import { JSXElement, ComponentSetup, Props } from '@viewfly/core'
 function replaceCSSClass(template, css: Record<string, string>) {
   if (template instanceof JSXElement) {
     const cssNames = template.props.attrs.get('css')
-    if (typeof cssNames !== 'string') {
-      return
-    }
     template.props.attrs.delete('css')
     const classes = template.props.classes
     Props.classToArray(cssNames).forEach(i => {

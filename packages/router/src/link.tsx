2fa95a78eb67c947445fc44ca874f02f3a4eca27
tbhuabi <464328895@qq.com>
2023-07-03 23:14:55 +0800

    feat: link 组件支持路由高亮匹配

--- packages/router/src/link.tsx
+++ packages/router/src/link.tsx
@@ -1,9 +1,11 @@
-import { inject, Props } from '@viewfly/core'
+import { inject, onDestroy, Props, useSignal } from '@viewfly/core'
 
 import { Navigator, QueryParams, Router } from './providers/_api'
 
 export interface LinkProps extends Props {
   to: string
+  active?: string
+  exact?: boolean
   queryParams?: QueryParams
   tag?: string
 }
@@ -11,6 +13,22 @@ export interface LinkProps extends Props {
 export function Link(props: LinkProps) {
   const navigator = inject(Navigator)
   const router = inject(Router)
+
+  function getActive() {
+    return props.exact ?
+      navigator.pathname === navigator.join(props.to, router) :
+      navigator.pathname.startsWith(navigator.join(props.to, router))
+  }
+
+  const isActive = useSignal(getActive())
+
+  const subscription = navigator.onUrlChanged.subscribe(() => {
+    isActive.set(getActive())
+  })
+
+  onDestroy(() => {
+    subscription.unsubscribe()
+  })
 
   function navigate(ev: Event) {
     ev.preventDefault()
@@ -28,6 +46,18 @@ export function Link(props: LinkProps) {
 
     if (Tag === 'a') {
       attrs.href = navigator.join(props.to, router, props.queryParams)
+    }
+
+    if (isActive() && props.active) {
+      if (!attrs.class) {
+        attrs.class = props.active.toString()
+      } else if (typeof attrs.class === 'string') {
+        attrs.class += ' ' + props.active
+      } else if (typeof props.active === 'object') {
+        attrs.class[props.active] = true
+      } else if (Array.isArray(attrs.class)) {
+        attrs.class.push(props.active)
+      }
     }
 
     return <Tag {...attrs}>{props.children}</Tag>

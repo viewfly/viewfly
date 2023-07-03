810b459b77b2f49ecc26297d238eeee6c4ee3d50
tbhuabi <464328895@qq.com>
2023-07-03 23:15:33 +0800

    feat: 调整脚手架名字

--- packages/cli/bin/cli.js
+++ packages/cli/bin/cli.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 import { Command } from 'commander'
-import {buildProject, packageVersion } from '../bundles/index.js'
+import { buildProject, packageVersion } from '../bundles/index.js'
 const program = new Command()
 
 
 program
   // 配置脚手架名称
-  .name('viewfly-cli')
+  .name('viewfly')
   // 配置命令格式
   .usage(`<command> [option]`)
   // 配置版本号

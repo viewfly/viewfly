dd56a8f1b3d676f8043af090e7b24ace4669b9fd
tbhuabi <464328895@qq.com>
2023-08-03 21:48:09 +0800

    docs: 修改文档注释错误

--- packages/platform-browser/src/create-app.ts
+++ packages/platform-browser/src/create-app.ts
@@ -5,15 +5,14 @@ import { DomRenderer } from './dom-renderer'
  * 创建一个 Viewfly 实例
  * @param root 应用根节点
  * @param autoUpdate 是否自动更新视图，默认为 true，当值为 false 时，Viewfly
- * 只会首次渲染，直到手动调用 Renderer 类的 refresh() 方法，这在单元测试中非常有用，
+ * 只会首次渲染，直到手动调用 app 的 render() 方法，这在单元测试中非常有用，
  * 我们无需等待 Viewfly 默认的异步调度，实现同步更新视图
  * ```tsx
  * const app = createApp(<App/>, false).mount(document.getElementById('app'))
- * const renderer = app.get(Renderer)
  *
  * // do something...
  *
- * renderer.refresh() // 手动更新视图
+ * app.render() // 手动更新视图
  * ```
  */
 export function createApp(root: JSXNode, autoUpdate = true) {

b6aa0bc5fb914ee2ce11b5bebc8156d6b19e64cd
tbhuabi <464328895@qq.com>
2024-01-11 20:36:32 +0800

    feat: 更新 cli 模板

--- packages/cli/src/package-template.ts
+++ packages/cli/src/package-template.ts
@@ -10,11 +10,11 @@ export function packageTemplate(name: string) {
     "test": "echo \\"Error: no test specified\\" && exit 1"
   },
   "dependencies": {
-    "@viewfly/core": "^0.2.4",
-    "@viewfly/hooks": "^0.2.4",
-    "@viewfly/platform-browser": "^0.2.4",
-    "@viewfly/router": "^0.2.4",
-    "@viewfly/scoped-css": "^0.2.4"
+    "@viewfly/core": "^0.6.0",
+    "@viewfly/hooks": "^0.6.0",
+    "@viewfly/platform-browser": "^0.6.0",
+    "@viewfly/router": "^0.6.0",
+    "@viewfly/scoped-css": "^0.6.0"
   },
   "devDependencies": {
     "@babel/core": "^7.16.5",
@@ -22,7 +22,7 @@ export function packageTemplate(name: string) {
     "@types/jest": "^29.5.2",
     "@typescript-eslint/eslint-plugin": "^5.8.0",
     "@typescript-eslint/parser": "^5.8.0",
-    "@viewfly/devtools": "^0.2.1",
+    "@viewfly/devtools": "^0.6.0",
     "autoprefixer": "^10.4.0",
     "babel-plugin-transform-es2015-modules-commonjs": "^6.26.2",
     "core-js": "^3.20.1",

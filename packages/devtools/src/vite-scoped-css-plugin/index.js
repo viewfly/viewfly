18ae394ca6d2f471740d3c67cb62791b61f76535
tbhuabi <464328895@qq.com>
2024-11-18 21:07:00 +0800

    feat: scoped css vite 插槽添加配置项

--- packages/devtools/src/vite-scoped-css-plugin/index.js
+++ packages/devtools/src/vite-scoped-css-plugin/index.js
@@ -65,9 +65,51 @@ function addScopedIdToCss() {
   }
 }
 
-export default function () {
-  return [
-    replaceImport(),
-    addScopedIdToCss(),
-  ]
+function scopedCss() {
+  return {
+    name: 'vite-plugin-scoped-css',
+    enforce: 'pre',
+    transform(rawCode, id) {
+      if (/node_modules/.test(id)) {
+        return
+      }
+
+      if (/(jsx?|tsx?)$/.test(id)) {
+        return rawCode.replace(
+          /import\s+(\w+)\s+from\s+['"]([.-\/\w]+\.scoped\.(s?css|stylus|styl|less))['"](?=[;\s])/g,
+          (_, $1, $2) => {
+            const p = path.join(id, '../', $2)
+            const scopedId = getScopedId(p)
+            return `import "${$2}";\nconst ${$1}= "${scopedId}";`
+          })
+      }
+
+      if (/\.scoped\.(s?css|stylus|styl|less)$/.test(id)) {
+        const scopedId = getScopedId(id)
+        const {code, map, errors} = compileStyle({
+          source: rawCode,
+          filename: id,
+          id: scopedId,
+          scoped: true,
+          trim: true,
+        })
+        if (errors.length) {
+          console.error(errors[0])
+        }
+        return {code, map}
+      }
+    }
+  }
 }
+
+
+export default function (inVue = false) {
+  if (inVue) {
+    return [
+      replaceImport(),
+      addScopedIdToCss(),
+    ]
+  }
+  return scopedCss()
+}
+
--- packages/devtools/vite-scoped-css-plugin/index.d.ts
+++ packages/devtools/vite-scoped-css-plugin/index.d.ts
@@ -1,3 +1,8 @@
 import { Plugin } from 'vite'
 
-export default function viteScopedCssPlugin(): Plugin
+/**
+ * 是否在 vue 项目中接入，默认为 false
+ * 如果在 vue 项目中，请传入 true
+ * @param inVue
+ */
+export default function viteScopedCssPlugin(inVue: boolean): Plugin

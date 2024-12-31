84b194160c265b64798e5426a0f7f99beadf649b
tbhuabi <464328895@qq.com>
2024-12-31 18:48:22 +0800

    feat: 添加 shallowReactive

--- index.tsx
+++ index.tsx
@@ -1,223 +1,52 @@
 import { createApp } from '@viewfly/platform-browser'
+import { reactive, shallowReactive } from '@viewfly/core'
 
-import './index.scss'
-import { computed, reactive } from '@viewfly/core'
-
-export interface Model {
-  id: number
-  label: string
-}
-
-const random = (max: number) => Math.round(Math.random() * 1000) % max
-
-const A = ['pretty', 'large', 'big', 'small', 'tall', 'short', 'long', 'handsome', 'plain', 'quaint', 'clean',
-  'elegant', 'easy', 'angry', 'crazy', 'helpful', 'mushy', 'odd', 'unsightly', 'adorable', 'important', 'inexpensive',
-  'cheap', 'expensive', 'fancy']
-const C = ['red', 'yellow', 'blue', 'green', 'pink', 'brown', 'purple', 'brown', 'white', 'black', 'orange']
-const N = ['table', 'chair', 'house', 'bbq', 'desk', 'car', 'pony', 'cookie', 'sandwich', 'burger', 'pizza', 'mouse',
-  'keyboard']
-
-let nextId = 1
-
-const buildData = (count: number) => {
-  const data = new Array(count)
-
-  for (let i = 0; i < count; i++) {
-    data[i] = {
-      id: nextId++,
-      label: `${A[random(A.length)]} ${C[random(C.length)]} ${N[random(N.length)]}`,
-    }
-  }
-
-  return data
-}
-const model = reactive({
-  rows: [] as Model[],
-  selected: null as number | null
-})
-
-function add() {
-  model.rows = model.rows.concat(buildData(1000))
-}
-
-function remove(id: number) {
-  model.rows.splice(
-    model.rows.findIndex((d) => d.id === id),
-    1
-  )
-}
-
-function select(id: number) {
-  model.selected = id
-}
-
-function run() {
-  model.rows = buildData(1000)
-  model.selected = null
-}
-
-function update() {
-  const _rows = model.rows
-  for (let i = 0; i < _rows.length; i += 10) {
-    _rows[i].label += ' !!!'
-  }
-}
-
-function runLots() {
-  model.rows = buildData(10000)
-  model.selected = null
-}
-
-function clear() {
-  model.rows = []
-  model.selected = null
-}
-
-function swapRows() {
-  const _rows = model.rows
-  if (_rows.length > 998) {
-    const d1 = _rows[1]
-    const d998 = _rows[998]
-    _rows[1] = d998
-    _rows[998] = d1
-  }
-}
-
-function Jumbotron() {
+function Child(props: any) {
   return () => {
+    console.log(props)
     return (
-      <div class="jumbotron">
-        <div class="row">
-          <div class="col-md-6">
-            <h1>Viewfly2 (keyed)</h1>
-          </div>
-          <div class="col-md-6">
-            <div class="row">
-              <div class="col-sm-6 smallpad">
-                <button
-                  type="button"
-                  class="btn btn-primary btn-block"
-                  id="run"
-                  onClick={run}
-                >
-                  Create 1,000 rows
-                </button>
-              </div>
-              <div class="col-sm-6 smallpad">
-                <button
-                  type="button"
-                  class="btn btn-primary btn-block"
-                  id="runlots"
-                  onClick={runLots}
-                >
-                  Create 10,000 rows
-                </button>
-              </div>
-              <div class="col-sm-6 smallpad">
-                <button
-                  type="button"
-                  class="btn btn-primary btn-block"
-                  id="add"
-                  onClick={add}
-                >
-                  Append 1,000 rows
-                </button>
-              </div>
-              <div class="col-sm-6 smallpad">
-                <button
-                  type="button"
-                  class="btn btn-primary btn-block"
-                  id="update"
-                  onClick={update}
-                >
-                  Update every 10th row
-                </button>
-              </div>
-              <div class="col-sm-6 smallpad">
-                <button
-                  type="button"
-                  class="btn btn-primary btn-block"
-                  id="clear"
-                  onClick={clear}
-                >
-                  Clear
-                </button>
-              </div>
-              <div class="col-sm-6 smallpad">
-                <button
-                  type="button"
-                  class="btn btn-primary btn-block"
-                  id="swaprows"
-                  onClick={swapRows}
-                >
-                  Swap Rows
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div>
+        {props.children}
       </div>
     )
   }
 }
 
-function Row(props: {row: Model}) {
-  const isSelected = computed(() => {
-    return model.selected === props.row.id
-  })
-  return () => {
-    console.log(333)
-    const { id, label } = props.row
-    return <tr class={{ danger: isSelected.value }}>
-      <td class="col-md-1">{id}</td>
-      <td class="col-md-4">
-        <a>{label}</a>
-      </td>
-      <td class="col-md-1">
-        <a>
-          <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-        </a>
-      </td>
-      <td class="col-md-6"></td>
-    </tr>
-  }
-}
+function App() {
+  const map = reactive(new Map<string, {name: string}>())
 
-function Table() {
+  let i = 0
   return () => {
-    console.log(444)
     return (
-      <table class="table table-hover table-striped test-data" onClick={(event) => {
-        // eslint-disable-next-line no-undef
-        const el = event.target as HTMLTableElement
-        const id = Number(el.closest('tr')!.firstChild!.textContent)
-        if (el.matches('.glyphicon-remove')) {
-          remove(id)
-        } else {
-          select(id)
-        }
-        return false
-      }}>
-        <tbody>
+      <div>
         {
-          model.rows.map(row => {
-            return <Row key={row.id} row={row}/>
+          Array.from(map.values()).map((item) => {
+            console.log(item)
+            return (
+              <div>
+                <p>{item.name}</p>
+                <div>
+                  <button onClick={() => {
+                    item.name = Math.random().toString(16)
+                  }}>
+                    changeName
+                  </button>
+                </div>
+              </div>
+            )
           })
         }
-        </tbody>
-      </table>
-    )
-  }
-}
-
-function App() {
-  return () => {
-    return (
-      <>
-        <Jumbotron/>
-        <Table/>
-        <span class="preloadicon glyphicon glyphicon-remove" aria-hidden="true"/>
-      </>
+        <Child>
+          <div>{map.size}</div>
+        </Child>
+        <div>
+          <button onClick={() => {
+            map.set('test' + i, { name: Math.random().toString() })
+            i++
+          }}>add
+          </button>
+        </div>
+      </div>
     )
   }
 }
--- packages/core/src/reactive/reactive.ts
+++ packages/core/src/reactive/reactive.ts
@@ -1,10 +1,13 @@
 import { makeError } from '../_utils/make-error'
 import { getStringType, hasOwn } from './_help'
 import { track, TrackOpTypes, trigger, TriggerOpTypes } from './dep'
+import { createArrayHandlers } from './array-handlers'
+import { createMapHandlers } from './map-handlers'
+import { createSetHandlers } from './set-handlers'
 
 const reactiveErrorFn = makeError('reactive')
-const rawToProxyCache = new WeakMap<object, any>()
-const proxyToRawCache = new WeakMap<object, any>()
+export const rawToProxyCache = new WeakMap<object, any>()
+export const proxyToRawCache = new WeakMap<object, any>()
 
 export function toRaw<T extends object>(value: T): T {
   if (proxyToRawCache.has(value)) {
@@ -15,258 +18,6 @@ export function toRaw<T extends object>(value: T): T {
 
 export function isReactive(value: any) {
   return proxyToRawCache.has(value)
-}
-
-const iterableIteratorMethods = {
-  * entries() {
-    const target = toRaw(this) as any
-    track(target, TrackOpTypes.Iterate)
-    for (const [key, value] of target.entries()) {
-      yield [reactive(key), reactive(value)]
-    }
-  },
-  * keys() {
-    const target = toRaw(this) as any
-    track(target, TrackOpTypes.Iterate)
-    for (const item of target.keys()) {
-      yield reactive(item)
-    }
-  },
-  * values() {
-    const target = toRaw(this) as any
-    track(target, TrackOpTypes.Iterate)
-    for (const item of target.values()) {
-      yield reactive(item)
-    }
-  }
-}
-
-const mapTypeInterceptors: Record<string | symbol, any> = {
-  get(this: any, key: any) {
-    const target = toRaw(this)
-    track(target, TrackOpTypes.Get, key)
-    return reactive(target.get(key))
-  },
-  set(this: any, key: any, value: any) {
-    const target = toRaw(this) as Map<any, any>
-    key = toRaw(key)
-    value = toRaw(value)
-    const has = target.has(key)
-    const r = target.set(key, value)
-    trigger(target, has ? TriggerOpTypes.Set : TriggerOpTypes.Add, key)
-    return r
-  },
-  has(this: any, key: any) {
-    const target = toRaw(this)
-    key = toRaw(key)
-    track(target, TrackOpTypes.Has, key)
-    return target.has(key)
-  },
-  delete(this: any, key: any) {
-    const target = toRaw(this)
-    key = toRaw(key)
-    const r = target.delete(key)
-    trigger(target, TriggerOpTypes.Delete, key)
-    return r
-  },
-  forEach(this: any, callbackFn: (value: any, key: any, map: Map<any, any>) => void, thisArg?: any) {
-    const target = proxyToRawCache.get(this) as Map<any, any>
-    track(target, TrackOpTypes.Iterate, undefined)
-    target.forEach((v, k, m) => {
-      callbackFn.call(this, reactive(v), reactive(k), m)
-    }, thisArg)
-  },
-  clear(this: any) {
-    const target = toRaw(this)
-    target.clear()
-    trigger(target, TriggerOpTypes.Clear, undefined)
-  },
-  [Symbol.iterator]() {
-    return this.entries()
-  },
-  ...iterableIteratorMethods
-}
-
-const setTypeInterceptors: Record<string | symbol, any> = {
-  add(this: any, value: any) {
-    const target = toRaw(this)
-    value = toRaw(value)
-    if (!target.has(value)) {
-      target.add(value)
-      trigger(target, TriggerOpTypes.Add, undefined)
-    }
-    return this
-  },
-  delete(this: any, value: any) {
-    const target = toRaw(this)
-    value = toRaw(value)
-    const has = target.has(value)
-    const b = target.delete(value)
-    if (!has) {
-      trigger(target, TriggerOpTypes.Delete, undefined)
-    }
-    return b
-  },
-  has(this: any, key: any) {
-    const target = toRaw(this)
-    key = toRaw(key)
-    track(target, TrackOpTypes.Has, key)
-    return target.has(key)
-  },
-  forEach(this: any, callbackFn: (value: any, value2: any, set: Set<any>) => void, thisArg?: any) {
-    const target = proxyToRawCache.get(this) as Set<any>
-    track(target, TrackOpTypes.Iterate, undefined)
-    target.forEach((v, k, m) => {
-      callbackFn.call(this, reactive(v), reactive(k), m)
-    }, thisArg)
-  },
-  clear(this: any) {
-    const target = toRaw(this)
-    const size = target.size
-    if (size !== 0) {
-      target.clear()
-      trigger(target, TriggerOpTypes.Clear, undefined)
-    }
-  },
-  [Symbol.iterator]() {
-    return this.values()
-  },
-  ...iterableIteratorMethods
-}
-
-function applyPredicateMethod(self: any, methodName: string, predicate: Function, thisArg?: any) {
-  const target = toRaw(self)
-  track(target, TrackOpTypes.Iterate)
-  return target[methodName]((value: unknown, index: number, array: unknown[]) => {
-    return predicate.call(target, reactive(value), index, array)
-  }, thisArg)
-}
-
-function applySearchMethod(self: any, methodName: string, searchElement: unknown, fromIndex?: number) {
-  const target = toRaw(self)
-  track(target, TrackOpTypes.Iterate)
-  return target[methodName](searchElement, fromIndex)
-}
-
-const arrayTypeInterceptors: Record<string | symbol, any> = {
-  concat(this: any, ...items: any[]): any[] {
-    const target = toRaw(this)
-    trigger(target, TriggerOpTypes.Add)
-    return target.concat(...items)
-  },
-  every(this: any,
-        predicate: (value: unknown, index: number, array: unknown[]) => unknown,
-        thisArg?: unknown) {
-    return applyPredicateMethod(this, 'every', predicate, thisArg)
-  },
-  filter(this: any, predicate: (value: unknown, index: number, array: unknown[]) => unknown, thisArg?: any) {
-    return applyPredicateMethod(this, 'filter', predicate, thisArg)
-  },
-  find(predicate: (value: unknown, index: number, obj: unknown[]) => unknown, thisArg?: any) {
-    return applyPredicateMethod(this, 'find', predicate, thisArg)
-  },
-  findIndex(predicate: (value: unknown, index: number, obj: unknown[]) => unknown, thisArg?: any): number {
-    return applyPredicateMethod(this, 'findIndex', predicate, thisArg)
-  },
-  findLast(predicate: (value: unknown, index: number, array: unknown[]) => unknown, thisArg?: any) {
-    return applyPredicateMethod(this, 'findLast', predicate, thisArg)
-  },
-  findLastIndex(predicate: (value: unknown, index: number, array: unknown[]) => unknown, thisArg?: any): number {
-    return applyPredicateMethod(this, 'findLastIndex', predicate, thisArg)
-  },
-  forEach(callbackfn: (value: unknown, index: number, array: unknown[]) => void, thisArg?: any) {
-    return applyPredicateMethod(this, 'forEach', callbackfn, thisArg)
-  },
-  includes(searchElement: unknown, fromIndex?: number): boolean {
-    return applySearchMethod(this, 'includes', searchElement, fromIndex)
-  },
-  indexOf(searchElement: unknown, fromIndex?: number): number {
-    return applySearchMethod(this, 'indexOf', searchElement, fromIndex)
-  },
-  join(separator?: string): string {
-    const target = toRaw(this)
-    track(target, TrackOpTypes.Iterate)
-    return target.join(separator)
-  },
-  lastIndexOf(searchElement: unknown, fromIndex?: number): number {
-    return applySearchMethod(this, 'lastIndexOf', searchElement, fromIndex)
-  },
-  map<U>(callbackFn: (value: unknown, index: number, array: unknown[]) => U, thisArg?: any): U[] {
-    return applyPredicateMethod(this, 'map', callbackFn, thisArg)
-  },
-  pop() {
-    const target = toRaw(this)
-    trigger(target, TriggerOpTypes.Delete)
-    return target.pop()
-  },
-  push(this: any, ...items: any[]) {
-    const target = toRaw(this)
-    trigger(target, TriggerOpTypes.Add)
-    return target.push(...items)
-  },
-  reduce(
-    callbackFn: (previousValue: unknown, currentValue: unknown, currentIndex: number, array: unknown[]) => unknown,
-    ...args: any[]) {
-    const target = toRaw(this)
-    track(target, TrackOpTypes.Iterate)
-    return target.reduce((p: unknown, c: unknown, i: number, a: unknown[]) => {
-      if (args.length > 0) {
-        return callbackFn(p, reactive(c), i, a)
-      }
-      return callbackFn(reactive(p), reactive(c), i, a)
-    }, ...args)
-  },
-  reduceRight(
-    callbackFn: (previousValue: unknown, currentValue: unknown, currentIndex: number, array: unknown[]) => unknown,
-    ...args: any[]) {
-    const target = toRaw(this)
-    track(target, TrackOpTypes.Iterate)
-    return target.reduceRight((p: unknown, c: unknown, i: number, a: unknown[]) => {
-      if (args.length > 0) {
-        return callbackFn(p, reactive(c), i, a)
-      }
-      return callbackFn(reactive(p), reactive(c), i, a)
-    }, ...args)
-  },
-  shift() {
-    const target = toRaw(this)
-    trigger(target, TriggerOpTypes.Delete)
-    return target.shift()
-  },
-  some(predicate: (value: unknown, index: number, array: unknown[]) => unknown, thisArg?: any): boolean {
-    return applyPredicateMethod(this, 'some', predicate, thisArg)
-  },
-  splice(start: number, deleteCount?: number) {
-    const target = toRaw(this) as unknown[]
-    trigger(target, TriggerOpTypes.Set)
-    trigger(target, TriggerOpTypes.Add)
-    trigger(target, TriggerOpTypes.Delete)
-    return target.splice(start, deleteCount).map(i => reactive(i))
-  },
-  toReversed() {
-    const target = toRaw(this)
-    track(target, TrackOpTypes.Iterate)
-    return target.toReversed()
-  },
-  toSorted(compareFn?: (a: unknown, b: unknown) => number) {
-    const target = toRaw(this)
-    track(target, TrackOpTypes.Iterate)
-    return target.toSorted(compareFn)
-  },
-  toSpliced(start: number, deleteCount: number, ...items: any[]) {
-    const target = toRaw(this)
-    track(target, TrackOpTypes.Iterate)
-    return target.toSpliced(start, deleteCount, ...items)
-  },
-  unshift(...items: any[]): number {
-    const target = toRaw(this)
-    trigger(target, TriggerOpTypes.Add)
-    return target.unshift(...items)
-  },
-  [Symbol.iterator]() {
-    return this.values()
-  },
-  ...iterableIteratorMethods
 }
 
 export interface ReactiveConfig {
@@ -333,19 +84,35 @@ export class ObjectReactiveHandler<T extends object> implements ProxyHandler<T> 
   }
 }
 
+function noReactive<T>(v: T): T {
+  return v
+}
+
 export class ArrayReactiveHandler extends ObjectReactiveHandler<any[]> {
+  interceptors = createArrayHandlers(this.isShallow ? noReactive : reactive)
+
+  constructor(config: ReactiveConfig) {
+    super(config)
+  }
+
   override get(target: any[], p: string | symbol, receiver: any): any {
-    if (Reflect.has(arrayTypeInterceptors, p) && p in target) {
-      return arrayTypeInterceptors[p]
+    if (Reflect.has(this.interceptors, p) && p in target) {
+      return (this.interceptors as any)[p]
     }
     return super.get(target, p, receiver)
   }
 }
 
 export class MapReactiveHandler extends ObjectReactiveHandler<Map<any, any> | WeakMap<object, any>> {
+  interceptors = createMapHandlers(this.isShallow ? noReactive : reactive)
+
+  constructor(config: ReactiveConfig) {
+    super(config)
+  }
+
   override get(target: Map<any, any> | WeakMap<object, any>, p: string | symbol, receiver: any) {
-    if (Reflect.has(mapTypeInterceptors, p) && p in target) {
-      return mapTypeInterceptors[p]
+    if (Reflect.has(this.interceptors, p) && p in target) {
+      return (this.interceptors as any)[p]
     }
     if (p === 'size') {
       track(target, TrackOpTypes.Iterate, p)
@@ -356,9 +123,15 @@ export class MapReactiveHandler extends ObjectReactiveHandler<Map<any, any> | We
 }
 
 export class SetReactiveHandler extends ObjectReactiveHandler<Set<any> | WeakSet<object>> {
+  interceptors = createSetHandlers(this.isShallow ? noReactive : reactive)
+
+  constructor(config: ReactiveConfig) {
+    super(config)
+  }
+
   override get(target: Set<any> | WeakSet<object>, p: string | symbol, receiver: any): any {
-    if (Reflect.has(setTypeInterceptors, p) && p in target) {
-      return setTypeInterceptors[p]
+    if (Reflect.has(this.interceptors, p) && p in target) {
+      return (this.interceptors as any)[p]
     }
     if (p === 'size') {
       track(target, TrackOpTypes.Iterate, p)
@@ -389,7 +162,7 @@ export const defaultSetReactiveHandler = new SetReactiveHandler({
 })
 
 export const readonlyProxyHandler = new ObjectReactiveHandler({
-  shallow: false,
+  shallow: true,
   readonly: true
 })
 
@@ -424,5 +197,3 @@ export function reactive<T>(raw: T): T {
   proxyToRawCache.set(proxy, raw)
   return proxy
 }
-
-

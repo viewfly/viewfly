487b6f3c5a1507ec11821c46d119e7f4053eeeca
tbhuabi <464328895@qq.com>
2025-02-18 16:31:05 +0800

    feat: 组件不再继承注入器

--- packages/core/src/base/context.ts
+++ packages/core/src/base/context.ts
@@ -1,34 +1,72 @@
-import { onPropsChanged } from './lifecycle'
-import { Provider } from '../di/_api'
-import { Props, jsx } from './jsx-element'
-import { withAnnotation } from './component'
+import {
+  AbstractType,
+  ExtractValueType,
+  InjectFlags,
+  InjectionToken,
+  Injector,
+  NullInjector,
+  Provider,
+  ReflectiveInjector,
+  Scope,
+  THROW_IF_NOT_FOUND,
+  Type
+} from '../di/_api'
+import { Props } from './jsx-element'
+import { Component, ComponentSetup, getCurrentInstance } from './component'
 
-export interface ContextProps extends Props {
-  providers: Provider[]
+const injectMap = new WeakMap<Component, ReflectiveInjector>()
+
+function getInjector(start: Component | null) {
+  while (start) {
+    const injector = injectMap.get(start)
+    if (injector) {
+      return injector
+    }
+    start = start.parentComponent
+  }
+  return new NullInjector()
 }
 
-export function Context(props: ContextProps) {
-  function createContextComponent(providers: Provider[]) {
-    return withAnnotation({
+export function createContext(providers: Provider[], scope?: Scope, parentInjector?: Injector) {
+  return function context(props: Props) {
+    const instance = getCurrentInstance()
+    const injector = new ReflectiveInjector(
+      parentInjector || getInjector(instance),
       providers,
-    }, (childProps: Props) => {
-      return () => {
-        return childProps.children
-      }
-    })
-  }
-
-  let contextComponent = createContextComponent(props.providers)
-
-  onPropsChanged((newProps: ContextProps, oldProps) => {
-    if (newProps.providers === oldProps.providers) {
-      return
+      scope
+    )
+    injectMap.set(instance, injector)
+    return () => {
+      return props.children
     }
-    contextComponent = createContextComponent(newProps.providers)
-  })
-  return () => {
-    return jsx(contextComponent, {
-      children: props.children
-    })
   }
+}
+
+
+/**
+ * 通过组件上下文获取 IoC 容器内数据的勾子方法
+ */
+export function inject<T extends Type<any> | AbstractType<any> | InjectionToken<any>, U = never>(
+  token: T,
+  notFoundValue: U = THROW_IF_NOT_FOUND as U,
+  flags?: InjectFlags,
+): ExtractValueType<T> | U {
+  const component = getCurrentInstance()
+  const injector = getInjector(component)
+  return injector.get(token, notFoundValue, flags)
+}
+
+export interface ComponentAnnotation {
+  scope?: Scope
+  providers?: Provider[]
+}
+
+export function withAnnotation<T extends ComponentSetup>(annotation: ComponentAnnotation, component: T): T {
+  return function (props: any) {
+    const instance = getCurrentInstance()
+    const parentInjector = injectMap.get(instance) || getInjector(instance.parentComponent)
+    const injector = new ReflectiveInjector(parentInjector, annotation.providers || [], annotation.scope)
+    injectMap.set(instance, injector)
+    return component(props)
+  } as T
 }

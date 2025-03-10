45339669db89c8f3a059f4bf8bcac4d1be05a833
tbhuabi <464328895@qq.com>
2025-03-10 10:10:24 +0800

    feat: 更新路由 hook

--- packages/router/src/hooks/use-query-params.ts
+++ packages/router/src/hooks/use-query-params.ts
@@ -1,19 +1,32 @@
-import { createSignal, inject, onUnmounted } from '@viewfly/core'
+import { comparePropsWithCallbacks, inject, internalWrite, onUnmounted, readonlyProxyHandler } from '@viewfly/core'
 
 import { Router } from '../providers/router'
 import { Navigator } from '../providers/navigator'
 import { UrlQueryParams } from '../providers/url-parser'
 
-export function useQueryParams<T extends UrlQueryParams>() {
+export function useQueryParams<T extends UrlQueryParams>(): T {
   const router = inject(Router)
   const navigator = inject(Navigator)
-  const queryParams = createSignal<T>(navigator.urlTree.queryParams as T)
+  const params: Record<string, any> = { ...navigator.urlTree.queryParams as T }
+  const queryParams = new Proxy(params, readonlyProxyHandler)
   const subscription = router.onRefresh.subscribe(() => {
-    queryParams.set(navigator.urlTree.queryParams as T)
+    comparePropsWithCallbacks(params, navigator.urlTree.queryParams, key => {
+      internalWrite(() => {
+        Reflect.deleteProperty(params, key)
+      })
+    }, (key, value) => {
+      internalWrite(() => {
+        params[key] = value
+      })
+    }, (key, value) => {
+      internalWrite(() => {
+        params[key] = value
+      })
+    })
   })
 
   onUnmounted(() => {
     subscription.unsubscribe()
   })
-  return queryParams
+  return queryParams as T
 }

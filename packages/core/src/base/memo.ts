b55faae4ee1c20b99bb5bb34163f46a075762527
tbhuabi <464328895@qq.com>
2026-04-20 00:17:38 +0800

    feat: 将 withMemo 票房为弃用

--- packages/core/src/base/memo.ts
+++ packages/core/src/base/memo.ts
@@ -1,6 +1,11 @@
 import { Props } from './jsx-element'
 import { ComponentInstance, JSXNode } from './component'
 
+/**
+ * @deprecated 即将弃用，Viewfly 默认就有 memo 的效果
+ * @param canUseMemo
+ * @param render
+ */
 export function withMemo<T extends Props = Props>(
   canUseMemo: ComponentInstance<T>['$useMemo'],
   render: () => JSXNode
